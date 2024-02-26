382b880d420301504218bdd99a90bf425b82e1e5
Alonso Cantu <a.cantu@uniandes.edu.co>
2024-02-26 17:11:47 -0600

    WIP create and get working

--- src/pda/config/db.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-
-db = None
-
-
-def init_db(app: Flask):
-    global db
-    db = SQLAlchemy(app)
--- src/pda/seedwork/domain/mixins.py
+++
@@ -1,8 +0,0 @@
-from .exceptions import BusinessRuleException
-from .rules import BusinessRule
-
-
-class ValidateMixinRules:
-    def validate_rule(self, rule: BusinessRule):
-        if not rule.is_valid():
-            raise BusinessRuleException(rule)
