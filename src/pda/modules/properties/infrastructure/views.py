c517706f8b211ea82c69270475ea6a9a63a02f05
Luis Guzman <luis.guzman@falcani.com>
2024-03-04 17:54:08 -0500

    Update parameter in TransactionView's get_by method
    The get_by method of TransactionView has been updated for better identification of parameters. It now receives id_transaction instead of simply id for better readability. The way id and id_client are passed into the params dictionary has also been corrected.

--- src/pda/modules/properties/infrastructure/views.py
+++ src/pda/modules/properties/infrastructure/views.py
@@ -5,13 +5,13 @@ from .dto import Transaction as TransactionDTO
 
 
 class TransactionView(View):
-    def get_by(id=None, id_client=None, **kwargs) -> [Transaction]:
+    def get_by(self, id_transaction=None, id_client=None, **kwargs) -> [Transaction]:
         params = dict()
 
-        if id:
-            params["id"] = str(id)
+        if id_transaction:
+            params["id"] = str(id_transaction)
 
         if id_client:
-            params["id_client"] = str(id_client)
+            params["id"] = str(id_client)
 
         return db.session.query(TransactionDTO).filter_by(**params)
